a2e4a91533236b9b0d824bf65756f690c8a7dfa5
Bayarsaikhan <bayarsaikhan@gerege.mn>
2023-03-22 15:13:39 +0800

    client, data, helpers, logger added

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package grc
 
-import "fmt"
+import (
+	"fmt"
 
-func HelloGRC() {
+	"github.com/spf13/viper"
+)
+
+func Hello() {
 	fmt.Println("Hello from GRC")
+}
+
+func LoadConfig(path string, conf interface{}) (c interface{}, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(conf)
+	c = conf
+	return c, err
 }
