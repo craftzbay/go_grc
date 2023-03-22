10bbb82fdc0b55ceffa7bc2180355aa4b3cddf6a
Bayarsaikhan <bayarsaikhan@gerege.mn>
2023-03-22 15:53:57 +0800

    data - pagination fixed

--- data/data_paginate.go
+++ data/data_paginate.go
@@ -4,19 +4,19 @@ type Pagination struct {
 	PageSize   uint        `json:"-"`
 	PageNumber uint        `json:"-"`
 	Offset     uint        `json:"-"`
-	TotalPage  uint        `json:"total_page"`
-	TotalRow   uint        `json:"total_row"`
+	TotalPage  int64       `json:"total_page"`
+	TotalRow   int64       `json:"total_row"`
 	Items      interface{} `json:"items"`
 }
 
-func Paginate(pageSize, pageNumber, totalRows uint) *Pagination {
+func Paginate(pageSize, pageNumber uint, totalRows int64) *Pagination {
 	offset := pageSize * (pageNumber - 1)
 	pagination := &Pagination{
 		PageSize:   pageSize,
 		PageNumber: pageNumber,
 		TotalRow:   totalRows,
 		Offset:     offset,
-		TotalPage:  (totalRows + pageSize - 1) / pageSize,
+		TotalPage:  (totalRows + int64(pageSize) - 1) / int64(pageSize),
 	}
 	return pagination
 }
